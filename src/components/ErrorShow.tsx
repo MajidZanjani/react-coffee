09d5afc779dae248be1e19f6b393ef0bb8f2f608
MajidZanjani <majidmmz1972@gmail.com>
2025-10-31 16:01:09 +0400

    feat: remove manu duplicate err element

--- src/components/ErrorShow.tsx
+++ src/components/ErrorShow.tsx
@@ -1,9 +1,7 @@
 export default function ErrorShow() {
   return (
-    <div>
-      <h1 className="text-bold text-2xl font-bold text-red-600">
-        Something's wrong with the API. Please refresh the page.
-      </h1>
+    <div className="flex flex-col text-bold text-2xl font-bold text-red-600 m-0 p-0 text-center">
+      <h1>Something's wrong with the API. Please refresh the page.</h1>
     </div>
   );
 }
