554adf1dcc244f7b035f41fdea3dad807944ac95
MajidZanjani <majidmmz1972@gmail.com>
2025-11-02 13:40:26 +0400

    feat: refactor product page

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import ProductList from "../components/ProductList";
+import ProductList from "./ProductList";
 import { fetchData } from "../api/fetchData";
 import ErrorShow from "../components/ErrorShow";
 
