ba4ed9d854686b95febb2081c81b0b3175cbd077
MajidZanjani <majidmmz1972@gmail.com>
2025-11-09 14:44:43 +0400

    feat: refactor header.tsx

---
+++ src/layouts/Header.tsx
@@ -0,0 +1,279 @@
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+
+interface User {
+  id: number;
+  login: string;
+  password: string;
+  confirmPassword: string;
+  city: string;
+  street: string;
+  houseNumber: number;
+  paymentMethod: string;
+}
+
+interface CartItem {
+  cartId: string;
+  id: number;
+  name: string;
+  price: string;
+  discountPrice: string;
+  size: string;
+  additives: string[];
+  image: string;
+}
+
+export default function Header() {
+  const storedUser = localStorage.getItem("user");
+  const user: User | null = storedUser ? JSON.parse(storedUser) : null;
+  const location = useLocation();
+  const [isMenupage, setIsMenupage] = useState(false);
+  const [cartExist, setCartExist] = useState(false);
+  const [cartSize, setCartSize] = useState(0);
+  const [discount, setDiscount] = useState(0);
+
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+    if (!menuOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "scroll";
+    }
+  };
+
+  useEffect(() => {
+    const updateCartSize = () => {
+      const cartStorage = localStorage.getItem("cart");
+      const cart: CartItem[] = cartStorage ? JSON.parse(cartStorage) : [];
+      setCartSize(cart.length);
+      setCartExist(cart.length > 0);
+      if (cartSize != 0) {
+        let dis = 0;
+        cart.forEach((item) => {
+          dis += Number(item.price) - Number(item.discountPrice);
+        });
+        setDiscount(dis);
+      }
+    };
+    // initial check for cartSize
+    updateCartSize();
+    // listen for updates across the app
+    window.addEventListener("cartUpdated", updateCartSize);
+    return () => {
+      window.removeEventListener("cartUpdated", updateCartSize);
+    };
+  }, [cartSize]);
+
+  useEffect(() => {
+    setIsMenupage(location.pathname.endsWith("menu.html"));
+  }, [location.pathname]);
+  return (
+    <header className="p-4 h-20 grid grid-flow-col gap-4 bg-text-light text-text-dark font-bold">
+      <Link
+        className="content-center grid grid-flow-col justify-items-start"
+        to="/"
+      >
+        <img src="/assets/images/logo.png" alt="logo" />
+      </Link>
+      <div
+        id="main-menu"
+        className="hidden content-center md:grid grid-flow-col gap-2 justify-items-center"
+      >
+        <a
+          className="transition-all duration-400 ease-in-out hover:text-text-accent"
+          href="#carousel"
+        >
+          Favorite coffee
+        </a>
+        <a
+          className="transition-all duration-400 ease-in-out hover:text-text-accent"
+          href="#about"
+        >
+          About
+        </a>
+        <a
+          className="transition-all duration-400 ease-in-out hover:text-text-accent"
+          href="#apps"
+        >
+          Mobile app
+        </a>
+        <a
+          className="transition-all duration-400 ease-in-out hover:text-text-accent"
+          href="#footer"
+        >
+          Contact us
+        </a>
+        <a
+          className={`transition-all duration-400 ease-in-out hover:text-text-accent ${
+            user ? "hidden" : ""
+          }`}
+          href="login.html"
+        >
+          Login
+        </a>
+        <a
+          className={`transition-all duration-400 ease-in-out hover:text-text-accent ${
+            user ? "hidden" : ""
+          }`}
+          href="register.html"
+        >
+          register
+        </a>
+        <a
+          className={`cursor-pointer text-red-700 transition-all duration-400 ease-in-out hover:text-red-400 ${
+            !user ? "hidden" : ""
+          }`}
+          onClick={() => {
+            localStorage.removeItem("user");
+            window.location.reload();
+          }}
+        >
+          Logout
+        </a>
+
+        {/* needs improvement for counter */}
+        <a href="cart.html" className={`${!user ? "hidden" : "flex gap-2"}`}>
+          <div className="count-icon">{cartExist ? String(cartSize) : ""}</div>
+          <div className="cart-icon">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 20 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M16.0942 8.36255L17.1455 15.1959C17.3319 16.4074 16.3945 17.5 15.1688 17.5H4.83122C3.60545 17.5 2.66809 16.4074 2.85448 15.1959L3.90576 8.36255C4.05586 7.38689 4.89536 6.66667 5.88251 6.66667H14.1175C15.1046 6.66667 15.9441 7.38689 16.0942 8.36255Z"
+                stroke="#403F3D"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M11.6668 4.16667C11.6668 3.24619 10.9206 2.5 10.0002 2.5C9.07969 2.5 8.3335 3.24619 8.3335 4.16667"
+                stroke="#403F3D"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+          <div className={`${discount != 0 ? "flex" : "hidden"}`}>
+            {discount.toFixed(2)}
+          </div>
+        </a>
+      </div>
+
+      <div className="hidden content-center md:grid grid-flow-col justify-items-end">
+        <Link
+          to="menu.html"
+          className={`${
+            isMenupage ? "pointer-events-none text-gray-400" : ""
+          } transition-all duration-400 ease-in-out hover:text-text-accent`}
+        >
+          Menu
+        </Link>
+      </div>
+
+      {/* burger menu, needs style improvement */}
+      <div className="md:hidden flex justify-end z-999">
+        <button
+          aria-label="Menu"
+          onClick={toggleMenu}
+          className="relative z-50 w-12 h-12 flex items-center justify-center rounded-full border border-gray-700 bg-background-body shadow-md transition-all duration-300"
+        >
+          <span
+            className={`absolute block h-0.5 w-6 bg-gray-800 transition-all duration-300 ${
+              menuOpen ? "rotate-45 translate-y-px" : "-translate-y-2"
+            }`}
+          ></span>
+          <span
+            className={`absolute block h-0.5 w-6 bg-gray-800 transition-all duration-300 ${
+              menuOpen ? "-rotate-45 -translate-y-px" : "translate-y-2"
+            }`}
+          ></span>
+        </button>
+
+        <nav
+          className={`fixed inset-0 bg-background-body flex flex-col items-center gap-6 text-lg font-semibold text-gray-800 transition-all duration-500 ${
+            menuOpen
+              ? "opacity-100 visible translate-x-0"
+              : "opacity-0 invisible translate-x-full"
+          }`}
+        >
+          <ul className="flex flex-col items-center mt-40 gap-6">
+            <li>
+              <a href="#carousel" onClick={toggleMenu}>
+                Favorite coffee
+              </a>
+            </li>
+            <li>
+              <a href="#about" onClick={toggleMenu}>
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#apps" onClick={toggleMenu}>
+                Mobile app
+              </a>
+            </li>
+            <li>
+              <a href="#footer" onClick={toggleMenu}>
+                Contact us
+              </a>
+            </li>
+            <li>
+              <a href="login.html" onClick={toggleMenu}>
+                Login
+              </a>
+            </li>
+            <li>
+              <a href="register.html" onClick={toggleMenu}>
+                Register
+              </a>
+            </li>
+            <li className="flex items-center gap-2">
+              <a href="menu.html" onClick={toggleMenu}>
+                Menu
+              </a>
+              <img
+                src="/assets/images/coffee-cup.png"
+                alt="coffee cup"
+                className="w-6 h-6"
+              />
+            </li>
+            <li className="flex items-center gap-2">
+              <a href="cart.html" className="cart-el-side" onClick={toggleMenu}>
+                Cart
+              </a>
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 20 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M16.0942 8.36255L17.1455 15.1959C17.3319 16.4074 16.3945 17.5 15.1688 17.5H4.83122C3.60545 17.5 2.66809 16.4074 2.85448 15.1959L3.90576 8.36255C4.05586 7.38689 4.89536 6.66667 5.88251 6.66667H14.1175C15.1046 6.66667 15.9441 7.38689 16.0942 8.36255Z"
+                  stroke="#403F3D"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M11.6668 4.16667C11.6668 3.24619 10.9206 2.5 10.0002 2.5C9.07969 2.5 8.3335 3.24619 8.3335 4.16667"
+                  stroke="#403F3D"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      {/* end of burger menu */}
+    </header>
+  );
+}
